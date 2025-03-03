307a713bff79f0714ba0d34ec9f4e46055b72e02
Diegogv23 <juandiguti@gmail.com>
2025-03-03 03:01:22 -0500

    Mejoras en el css de index.php

--- index.php
+++ index.php
@@ -52,83 +52,91 @@ session_start(); // Iniciar sesión si hay autenticación
 
     </section>
 
-    <main>
+    <main class="contenedor sombra">
         <h2>Nuestros servicios</h2>
 
-        <section>
-            <h3>Facturacion electronica</h3>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" width="36" height="36" stroke-width="2">
-                <path d="M14 3v4a1 1 0 0 0 1 1h4"></path>
-                <path d="M17 21h-10a2 2 0 0 1 -2 -2v-14a2 2 0 0 1 2 -2h7l5 5v11a2 2 0 0 1 -2 2z"></path>
-                <path d="M9 7l1 0"></path>
-                <path d="M9 13l6 0"></path>
-                <path d="M13 17l2 0"></path>
-            </svg>
-            <p>adipisicing elit. Dolor, error dignissimos consequatur distinctio tempore consectetur
-                laborum quas! Ratione quibusdam
-                tempora odio veniam dolore sapiente facere possimus rem! Enim, eligendi quos!</p>
-        </section>
+        <div class="servicios">
+
+            <section class="servicio">
+                <h3>Facturacion electronica</h3>
+                <div class="iconos">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" width="36" height="36" stroke-width="2">
+                        <path d="M14 3v4a1 1 0 0 0 1 1h4"></path>
+                        <path d="M17 21h-10a2 2 0 0 1 -2 -2v-14a2 2 0 0 1 2 -2h7l5 5v11a2 2 0 0 1 -2 2z"></path>
+                        <path d="M9 7l1 0"></path>
+                        <path d="M9 13l6 0"></path>
+                        <path d="M13 17l2 0"></path>
+                    </svg>
+                </div>
+                <p>adipisicing elit. Dolor, error dignissimos consequatur distinctio tempore consectetur
+                    laborum quas! Ratione quibusdam
+                    tempora odio veniam dolore sapiente facere possimus rem! Enim, eligendi quos!</p>
+            </section>
+
+            <section class="servicio">
+                <h3>Control de inventario</h3>
+                <div class="iconos">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" width="36" height="36" stroke-width="2">
+                        <path d="M3 21v-13l9 -4l9 4v13"></path>
+                        <path d="M13 13h4v8h-10v-6h6"></path>
+                        <path d="M13 21v-9a1 1 0 0 0 -1 -1h-2a1 1 0 0 0 -1 1v3"></path>
+                    </svg>
+                </div>
+                <p>Ipsam atque, ducimus, a illum cumque quos quam asperiores sequi fuga sunt libero minus
+                    dolor inventore doloribus delectus
+                    deserunt magnam eos recusandae?</p>
+            </section>
+
+            <section class="servicio">
+                <h3>Facil administracion</h3>
+                <div class="iconos">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" width="36" height="36" stroke-width="2">
+                        <path d="M11.537 19.542l-7.037 -6.97a5 5 0 1 1 7.5 -6.566a5 5 0 0 1 8.212 5.693"></path>
+                        <path d="M19 22.5a4.75 4.75 0 0 1 3.5 -3.5a4.75 4.75 0 0 1 -3.5 -3.5a4.75 4.75 0 0 1 -3.5 3.5a4.75 4.75 0 0 1 3.5 3.5"></path>
+                    </svg>
+                </div>
+                <p>Non sed impedit dolorem accusamus eaque, placeat architecto, ducimus mollitia eos
+                    molestias aliquid. Tenetur reiciendis autem architecto
+                    rerum placeat nisi voluptatibus officiis.</p>
+            </section>
+        </div>
+
 
         <section>
-            <h3>Control de inventario</h3>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" width="36" height="36" stroke-width="2">
-                <path d="M3 21v-13l9 -4l9 4v13"></path>
-                <path d="M13 13h4v8h-10v-6h6"></path>
-                <path d="M13 21v-9a1 1 0 0 0 -1 -1h-2a1 1 0 0 0 -1 1v3"></path>
-            </svg>
-            <p>Ipsam atque, ducimus, a illum cumque quos quam asperiores sequi fuga sunt libero minus
-                dolor inventore doloribus delectus
-                deserunt magnam eos recusandae?</p>
-        </section>
+            <h2>Contacto</h2>
 
-        <section>
-            <h3>Facil administracion</h3>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" width="36" height="36" stroke-width="2">
-                <path d="M11.537 19.542l-7.037 -6.97a5 5 0 1 1 7.5 -6.566a5 5 0 0 1 8.212 5.693"></path>
-                <path d="M19 22.5a4.75 4.75 0 0 1 3.5 -3.5a4.75 4.75 0 0 1 -3.5 -3.5a4.75 4.75 0 0 1 -3.5 3.5a4.75 4.75 0 0 1 3.5 3.5"></path>
-            </svg>
-            <p>Non sed impedit dolorem accusamus eaque, placeat architecto, ducimus mollitia eos
-                molestias aliquid. Tenetur reiciendis autem architecto
-                rerum placeat nisi voluptatibus officiis.</p>
-        </section>
+            <form class="formulario">
+                <fieldset>
+                    <legend>Contactanos llenando todos los campos</legend>
 
+                    <div>
+                        <label>Nombre</label>
+                        <input type="text" placeholder="Tu Nombre">
+                    </div>
+
+                    <div>
+                        <label>Telefono</label>
+                        <input type="text" placeholder="Tu Telefono">
+                    </div>
+
+                    <div>
+                        <label>Correo</label>
+                        <input type="email" placeholder="Mail">
+                    </div>
+
+                    <div>
+                        <label>Mensaje</label>
+                        <textarea></textarea>
+                    </div>
+                    <div>
+                        <input type="submit" value="Enviar">
+                    </div>
+
+                </fieldset>
+            </form>
+
+        </section>
     </main>
-
-    <section>
-        <h2>Contacto</h2>
-
-        <form>
-            <fieldset>
-                <legend>Contactanos llenando todos los campos</legend>
-
-                <div>
-                    <label>Nombre</label>
-                    <input type="text" placeholder="Tu Nombre">
-                </div>
-
-                <div>
-                    <label>Telefono</label>
-                    <input type="text" placeholder="Tu Telefono">
-                </div>
-
-                <div>
-                    <label>Correo</label>
-                    <input type="email" placeholder="Mail">
-                </div>
-
-                <div>
-                    <label>Mensaje</label>
-                    <textarea></textarea>
-                </div>
-                <div>
-                    <input type="submit" value="Enviar">
-                </div>
-
-            </fieldset>
-        </form>
-
-    </section>
-
     <footer>
         <p>&copy; 2025 Proyecto MIG (SENA). Todos los derechos reservados.</p>
     </footer>
